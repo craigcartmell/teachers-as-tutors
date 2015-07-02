07c9fb9b4cc68e525a93da4d355ef727f882ebf6
Craig Cartmell <craigcartmell1@gmail.com>
2015-07-02 20:18:34 +0100

    Update created_by and updated_by in model event

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,12 @@
 namespace TeachersAsTutors\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use TeachersAsTutors\Page;
+use TeachersAsTutors\User;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $models = [User::class, Page::class];
     /**
      * Bootstrap any application services.
      *
@@ -13,7 +16,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        foreach($this->models as $model) {
+            $model::creating(function ($model) {
+                $model->created_by = auth()->user()->getKey();
+            });
+
+            $model::updating(function ($model) {
+                $model->updated_by = auth()->user()->getKey();
+            });
+        }
     }
 
     /**
